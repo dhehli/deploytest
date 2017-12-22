0481d54646bdbf22f4a5a739b998b1cfa27d07a9
dhehli <dh@netlive.ch>
2017-12-22 15:44:38 +0100

    add db conn

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express')
-const app = express()
+const app = express();
+const r = require('rethinkdb')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+function getData(cb){
+    var connection = null;
+    r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+        if (err) throw err;
+        connection = conn;
+
+        r.db('test').table('authors').run(connection, function(err, cursor) {
+          if (err) throw err;
+          cursor.toArray(function(err, result) {
+              cb(result)
+          });
+        })
+    })
+}
+
+app.get('/', (req, res) => {
+    getData(function(result){
+        return res.json({result})
+    })
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
